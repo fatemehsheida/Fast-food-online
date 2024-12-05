c3a1d7ae085458f842274de547278e70040f3fa2
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-12-05 17:41:50 +0330

    complect db.json

--- src/components/ContainerFood.tsx
+++ src/components/ContainerFood.tsx
@@ -1,9 +1,15 @@
-export default function ContainerFood(){
-    return(
-        <div className="bg-slate-300 w-full h-[85%] absolute top-16 px-20 py-2">
-            <div className="text-neutral-800 font-bold text-2xl text-right"> <p>منو</p></div>
-            <div></div>
-            <div></div>
-        </div>
-    )
+
+
+
+export default function ContainerFood() {
+  return (
+    <div className="bg-slate-100 w-full h-[85%] absolute top-16 px-20 py-2">
+      <div className="text-neutral-800 font-bold text-2xl text-right">
+        {" "}
+        <p>منو</p>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
